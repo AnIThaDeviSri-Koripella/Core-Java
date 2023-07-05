86f9c43a63a5355073aefb165cef60508b74880f
AnIThaDeviSri-Koripella <anithakoripella@gmail.com>
2023-07-05 20:31:58 +0530

    Object creation,imports class from another package

---
+++ Demo.java
@@ -0,0 +1,12 @@
+import tools.Calcul;
+
+public class Demo 
+{
+    public static void main(String args[])
+    {
+        Calcul obj = new Calcul();
+        int r1 = obj.add(2,3);
+        System.out.println(r1);
+    }
+    
+}
