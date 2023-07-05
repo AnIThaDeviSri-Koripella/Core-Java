54876458bcd7464f2d192edc2b99ac3660e55f05
AnIThaDeviSri-Koripella <anithakoripella@gmail.com>
2023-07-05 19:46:44 +0530

    Promoting one datatype to another datatype

---
+++ Typepromotion.java
@@ -0,0 +1,13 @@
+class Typepromotion 
+{
+    public static void main(String args[])
+    {
+        byte a = 10;
+        byte b = 30;
+        int c;
+        c= a*b;
+        System.out.println(c);
+    }
+
+
+}
