f199521b4569c8582eec6485dfb1e34d91483665
AnIThaDeviSri-Koripella <anithakoripella@gmail.com>
2023-07-05 20:16:53 +0530

    Multiplication Table

---
+++ Table.java
@@ -0,0 +1,15 @@
+
+
+public class Table 
+{
+    public static void main(String args[])
+    {
+        int i=1,num=9;
+        while(i<=10)
+        {
+            System.out.printf("%d * %d = %d\n",num,i,num*i);
+            i++;
+        }
+    }
+    
+}
