426760aad2ee8889337856308c5f06edf9713326
AnIThaDeviSri-Koripella <anithakoripella@gmail.com>
2023-07-05 19:41:08 +0530

    Type Conversion

---
+++ Conversion.java
@@ -0,0 +1,20 @@
+class Conversion 
+{
+    public static void main(String args[])
+    {
+        byte b =127;
+        int a = 12;
+        byte k = (byte)a;
+        System.out.println(k);
+        System.out.println(b);
+
+        float f = 5.6f;
+        int t = (int)f;
+        System.out.println(t);
+        
+        byte by = 123;
+        int x = (int)(by*2);
+        System.out.println(x);
+    }
+}
+
