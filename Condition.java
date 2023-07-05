2dd4ee973018cdbf9d67242ceb5247472b71a3df
AnIThaDeviSri-Koripella <anithakoripella@gmail.com>
2023-07-05 19:54:20 +0530

    if-else condition

---
+++ Condition.java
@@ -0,0 +1,24 @@
+
+public class Condition 
+{
+    public static void main(String args[])
+    {
+        int x = 322;
+        int y = 23;
+        int z = 54;
+        if(x>y && x>z)
+        {
+            System.out.println(x);
+            System.out.println("thank you");
+        }
+        else if(y>z)
+        {
+            System.out.println(y);
+            System.out.println("thank you");
+        }
+        else
+            System.out.println("Bigger number is " +z);
+            System.out.println("thank you");
+    }
+    
+}
