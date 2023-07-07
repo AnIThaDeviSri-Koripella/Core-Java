3b766590de18ac2c266b9d25d5948dbf21fe27d7
AnIThaDeviSri-Koripella <anithakoripella@gmail.com>
2023-07-07 16:16:12 +0530

    Ternary operator similar to if else

---
+++ Ternary.java
@@ -0,0 +1,12 @@
+
+public class Ternary 
+{
+    public static void main(String args[])
+    {
+        int x = 7;
+        int result;
+        result = (x%2!=0)?(x==7?354:56):20;
+        System.out.println(result);
+    }
+    
+}
